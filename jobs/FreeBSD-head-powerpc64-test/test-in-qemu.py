0e59badaad6d983616f3f7d0d0edefcf82ef1551
Alfredo Dal'Ava Junior <alfredo@freebsd.org>
2020-02-11 13:44:09 -0200

    fixup! aa

--- jobs/FreeBSD-head-powerpc64-test/test-in-qemu.py
+++ jobs/FreeBSD-head-powerpc64-test/test-in-qemu.py
@@ -19,14 +19,14 @@ def forsend(child, s):
 
 cmd = "qemu-system-ppc64 \
 -name {} \
--smp 2 \
+-smp {} \
 -M pseries,cap-cfpc=broken,cap-sbbc=broken,cap-ibs=broken,cap-hpt-max-page-size=16M \
 -m {} \
--drive file={},if=scsi,index=0,format=raw \
--drive file=meta.tar \
+-drive file=meta.tar,format=raw,if=virtio
+-drive file={},if=virtio,index=0,format=raw \
 -nographic -vga none \
 -netdev user,id=mynet0 -device spapr-vlan,netdev=mynet0\
-".format(TEST_VM_NAME, VM_MEM_SIZE, IMG_NAME)
+".format(TEST_VM_NAME, VM_CPU_COUNT, VM_MEM_SIZE, IMG_NAME)
 
 print(cmd)
 
@@ -34,11 +34,11 @@ child = pexpect.spawn(cmd)
 child.logfile = sys.stdout
 child.delaybeforesend = 0.5
 
-#child.expect(re.compile("^login:", re.MULTILINE), timeout=600)
-#forsend(child, "root")
+child.expect(re.compile("^login:", re.MULTILINE), timeout=600)
+forsend(child, "root")
 
-#child.expect("#", timeout=300)
-#forsend(child, "shutdown -p now")
+child.expect("#", timeout=300)
+forsend(child, "shutdown -p now")
 
 #wait up to 8 hours
 child.expect("Uptime:.*", timeout=28800)
